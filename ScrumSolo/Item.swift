767aca80593d98c463b011654d9c6ca1078a3114
Baoli Zhai <horidream@gmail.com>
2017-05-23 01:08:39 +0800

    change pod Shift 0.1.1

--- ScrumSolo/Item.swift
+++ ScrumSolo/Item.swift
@@ -13,7 +13,7 @@ import CloudKit
 
 
 class Item: LocalManageable, CloudManageable, CustomStringConvertible{
-
+    
     var id:Int64?
     var record:CKRecord?
     var tableName:String { return "items" }
@@ -31,7 +31,7 @@ class Item: LocalManageable, CloudManageable, CustomStringConvertible{
     required init(_ rst: FMResultSet) {
         self.id = rst.longLongInt(forColumn: "id")
     }
-
+    
     required init(_ record:CKRecord){
         self.record = record
     }
@@ -42,16 +42,16 @@ class Item: LocalManageable, CloudManageable, CustomStringConvertible{
             _ = localStorage.exe("delete from items where id = ?", args: [id])
         }
     }
-
+    
     func save() {
         if let id = id,  localStorage.rowExists(id: id){
             update()
-//            _ = localStorage.exe("update items set title=? where id=?", args: [title, id])
+            //            _ = localStorage.exe("update items set title=? where id=?", args: [title, id])
         }else{
             self.id = create()
-//            if localStorage.exe("insert into items (title) values(?)", args: [title]){
-//                self.id = localStorage.lastInsertRowId
-//            }
+            //            if localStorage.exe("insert into items (title) values(?)", args: [title]){
+            //                self.id = localStorage.lastInsertRowId
+            //            }
         }
     }
     
@@ -94,8 +94,8 @@ class Item: LocalManageable, CloudManageable, CustomStringConvertible{
             }
         }
     }
-
-   
+    
+    
     var description: String{
         return "Item - \(id ?? 0)"
     }
@@ -103,67 +103,3 @@ class Item: LocalManageable, CloudManageable, CustomStringConvertible{
 
 
 
-
-protocol Composite{
-    associatedtype T
-    var children:[T] { get set }
-    var parent:T? { get }
-    
-    func addChild(_ child:T)
-    func insertChild(_ child:T, at index:Int)
-    func removeChild(at index:Int)
-    func removeAllChild()
-    func removeFromParent()
-}
-
-extension Composite{
-    mutating func addChild(_ child:T){
-        children.append(child)
-    }
-
-    mutating func insertChild(_ child:T, at index:Int)
-    {
-        children.insert(child, at: index)
-    }
-    
-    mutating func removeChild(at index:Int){
-        if( index < children.count && index >= 0){
-            children.remove(at: index)
-        }
-    }
-    
-    mutating func removeAllChild(){
-        children = []
-    }
-}
-
-extension Composite where T:Equatable,T:Composite{
-    func removeChild(_ child:T){
-        if let index = children.index(where: { $0 == child}){
-            removeChild(at: index)
-        }
-    }
-    
-    func removeFromParent(){
-    }
-
-}
-
-
-//class ScrumItem:Item{
-//    var title:String
-//    init(title:String){
-//        self.title = title
-//        super.init()
-//    }
-//    
-//    required init(_ record: CKRecord) {
-//        self.title = record["title"] as? String ?? ""
-//        super.init(record)
-//    }
-//    
-//    required init(_ rst: FMResultSet) {
-//        self.title = rst.string(forColumn: "title")
-//        super.init(rst)
-//    }
-//}
